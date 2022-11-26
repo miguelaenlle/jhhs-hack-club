ea0603f02cefd78cb40d6042839de6fed9c5e560
Alex Strugacz <alexstrugacz@icloud.com>
2022-11-26 01:47:29 -0600

    showcase animations

--- src/components/showcase/Showcase.tsx
+++ src/components/showcase/Showcase.tsx
@@ -5,17 +5,30 @@ import ShowcaseItem from "../showcase/ShowcaseItem";
 import { AnimatedBg } from "../shared/AnimatedBg";
 import BackButton from "../shared/BackButton";
 import GradientContent from "../shared/GradientContent";
+import { motion } from "framer-motion";
 
 const Showcase: React.FC<{}> = (props) => {
   return (
     <div className="bg-gradient-to-b from-neutral-800 to-neutral-900 p-5 min-h-screen">
       <BackButton link={"/"} />
       <div className={"pt-4 px-10"}>
-        <div className={"pt-6 pb-24"}>
-          <h1 className={"text-center text-4xl sm:text-5xl font-bold font-Poppins text-red-500 mb-5"}>Showcase</h1>
-          <p className={"text-center text-xl font-Poppins text-zinc-300 mt-3 max-w-2xl mx-auto"}>
-            Check out the awesome projects built at Hersey Hack Club!
-          </p>
+        <div className={"pt-6 pb-16"}>
+          <motion.h1
+            initial={{ opacity: 0, y: -20, scale: 0.8 }}
+            animate={{ opacity: 1, y: 0, scale: 1 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            className={"text-center text-4xl sm:text-6xl font-bold font-Poppins text-red-500 mb-5"}>
+            Showcase
+          </motion.h1>
+          <motion.p
+
+            initial={{ opacity: 0, y: -20, scale: 0.8 }}
+            animate={{ opacity: 1, y: 0, scale: 1 }}
+            transition={{ delay: 0.8, duration: 0.5 }}
+            className={"text-center text-zinc-300 font-Poppins max-w-lg mx-auto text-lg pt-2"}>
+            Check out some of the amazing projects that have been built @ Hersey Hack Club!
+
+          </motion.p>
         </div>
       </div>
       <div className={"flex flex-col align-center w-200 gap-5"}>
