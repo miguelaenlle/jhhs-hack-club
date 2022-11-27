8db8b98fe2c8d17347fc7568cd89c5a1de3450a7
Alex Strugacz <alexstrugacz@icloud.com>
2022-11-27 01:45:31 -0600

    add submit-your-project form

--- src/components/showcase/Showcase.tsx
+++ src/components/showcase/Showcase.tsx
@@ -9,7 +9,7 @@ const Showcase: React.FC<{}> = (props) => {
     <div className="bg-gradient-to-b from-neutral-800 to-neutral-900 p-5 min-h-screen">
       <BackButton link={"/"} />
       <div className={"pt-4 px-10"}>
-        <div className={"pt-6 pb-16"}>
+        <div className={"pt-6 pb-4"}>
           <motion.h1
             initial={{ opacity: 0, y: -20, scale: 0.8 }}
             animate={{ opacity: 1, y: 0, scale: 1 }}
@@ -28,24 +28,30 @@ const Showcase: React.FC<{}> = (props) => {
           </motion.p>
         </div>
       </div>
-      <div className={"flex flex-col align-center w-200 gap-5"}>
-        <div className="bg-gradient-to-b from-neutral-800 to-neutral-900 p-5 min-h-screen">
-          <br />
-          <div className={"m-10 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4"}>
-            {showcase.map(showcase => {
-              return (
-                <ShowcaseItem
-                  title={showcase.title}
-                  backgroundColor={showcase.backgroundColor}
-                  link={showcase.link}
-                  author={showcase.author}
-                />
-              )
-            })}
-          </div>
+      <div className={"flex flex-col items-center w-200 gap-5"}>
+
+        <br />
+
+        <div className="flex flex-col gap-5">
+          <h2 className=" text-center text-3xl font-Poppins font-bold text-white">Submit your Project to the Showcase!</h2>
+          <iframe className="rounded" width="600" height="300" src="https://1htona3vpjh.typeform.com/to/fuR1LL5n"></iframe>
+        </div>
+        <br />
+        <div className={"m-10 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4"}>
+          {showcase.map(showcase => {
+            return (
+              <ShowcaseItem
+                title={showcase.title}
+                backgroundColor={showcase.backgroundColor}
+                link={showcase.link}
+                author={showcase.author}
+              />
+            )
+          })}
         </div>
       </div>
     </div>
+
   );
 };
 
