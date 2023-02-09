d0d0bbefd21b4d5a31993d926119ff36b85696e1
Alex Strugacz <90818590+Nectar0@users.noreply.github.com>
2023-02-08 20:13:18 -0600

    merch survey button

--- src/components/landing/intro/IntroSection.tsx
+++ src/components/landing/intro/IntroSection.tsx
@@ -54,6 +54,26 @@ const IntroSection: React.FC<{}> = (props) => {
               startColor={"from-cyan-500"}
               endColor={"to-cyan-700"}
             />
+            <a
+              href='https://forms.gle/h3hZ2DUEe6edzSJNA'
+              className={
+                `bg-gradient-to-br from-purple-400 to-purple-700 rounded-full p-3 px-7 w-fit hover:cursor-pointer hover:scale-105 transition-all`
+              }
+            >
+              <div className='flex'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="yellow" viewBox="0 0 24 24" strokeWidth={1.5} stroke="yellow" className="mr-2 w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                </svg>
+
+                <p
+                  className={
+                    "text-md text-white font-Poppins font-bold"
+                  }
+                >
+                  Merch Interest Form
+                </p>
+              </div>
+            </a>
           </motion.div>
         </div>
       </div>
