c0b37b5c90cad88523c831543a0ad7498bf342cb
nexus1010 <bot.developer3@gmail.com>
2023-03-01 18:06:58 -0600

    added hshacks hackathon button

--- src/components/landing/intro/IntroSection.tsx
+++ src/components/landing/intro/IntroSection.tsx
@@ -55,23 +55,21 @@ const IntroSection: React.FC<{}> = (props) => {
               endColor={"to-cyan-700"}
             />
             <a
-              href='https://forms.gle/h3hZ2DUEe6edzSJNA'
+              href='https://hshacks.org/'
               className={
-                `bg-gradient-to-br from-green-400 to-green-700 rounded-full p-3 px-7 w-fit hover:cursor-pointer hover:scale-105 transition-all`
+                `bg-gradient-to-br from-[#0C6885] to-[#5E0D84] rounded-full p-3 px-5 w-fit hover:cursor-pointer hover:scale-105 transition-all`
               }
             >
-              <div className='flex'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="yellow" viewBox="0 0 24 24" strokeWidth={1.5} stroke="yellow" className="mr-2 w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-                </svg>
-
+              <div className='flex items-center gap-2'>
+                <img src={"https://hshacks.org/static/media/logo.bc36ba9c0dca76280398.PNG"} className={"h-7"} />
                 <p
                   className={
                     "text-md text-white font-Poppins font-bold"
                   }
                 >
-                  Merch Order Form
+                  HSHacks Hackathon
                 </p>
+                <p className={"text-green-500 mb-[2px]"}>NEW</p>
               </div>
             </a>
           </motion.div>
