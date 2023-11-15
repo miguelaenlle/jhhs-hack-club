10b05723529a6029d6673de9893b5ad18f15636f
nexus1010 <bot.developer3@gmail.com>
2023-11-15 17:55:19 -0600

    Added more site changes

--- src/components/workshops/Workshop.tsx
+++ src/components/workshops/Workshop.tsx
@@ -5,17 +5,40 @@ import MarkdownPreview from '@uiw/react-markdown-preview';
 import { useWorkshopHook } from "./hooks/use-workshop-hook";
 import { AnimatedBg } from "../shared/AnimatedBg";
 import '@hackclub/theme/fonts/reg-bold.css'
+import { motion } from "framer-motion";
 
 const Workshop: React.FC<{}> = (props) => {
     const workshopHook = useWorkshopHook();
 
     return (
         <div className={`bg-gradient-to-b bg-zinc-900 min-h-screen font-Poppins`}>
-            <AnimatedBg className={"pt-5 px-10"}>
+            <AnimatedBg className={"pt-5 px-5"}>
                 <BackButton link={"/workshops"} />
-                <div className={"pt-12 pb-24"}>
-                    <h3 className={"text-center text-5xl text-white font-bold"}>{workshopHook.title}</h3>
-                    <p className={"text-center text-lg md:text-xl font-Poppins text-zinc-100 mt-3 max-w-2xl mx-auto"}>{workshopHook.description}</p>
+                <div className={"pt-24 pb-32 flex flex-col items-start max-w-4xl md:px-10 mx-auto"}>
+
+
+                    <motion.p
+                        initial={{ opacity: 0, y: -20, scale: 1.1 }}
+                        animate={{ opacity: 1, y: 0, scale: 1 }}
+                        transition={{ delay: 0.2, duration: 0.5 }}
+                        className={"text-left text-lg text-zinc-200 font-bold mb-3"}>
+                        💻 WORKSHOP
+                    </motion.p>
+                    <motion.h3
+                        initial={{ opacity: 0, y: -20, scale: 1.2 }}
+                        animate={{ opacity: 1, y: 0, scale: 1 }}
+                        transition={{ delay: 0.7, duration: 0.5 }}
+                        className={"text-left text-5xl text-white font-bold"}>
+                        {workshopHook.title}
+                    </motion.h3>
+
+                    <motion.p
+                        initial={{ opacity: 0, y: -20, scale: 1.2 }}
+                        animate={{ opacity: 1, y: 0, scale: 1 }}
+                        transition={{ delay: 1.2, duration: 0.5 }}
+                        className={"text-left text-lg md:text-xl font-Poppins text-zinc-100 mt-3 max-w-2xl"}>
+                        {workshopHook.description}
+                    </motion.p>
                 </div>
             </AnimatedBg>
             <MarkdownPreview warpperElement={{
